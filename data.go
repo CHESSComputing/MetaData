38b0c7ececc3555822d0c18e24c96f958d592d64
Valentin Kuznetsov <vkuznet@gmail.com>
2025-04-18 15:39:27 -0400

    Add default doi attributes values

--- data.go
+++ data.go
@@ -114,6 +114,16 @@ func insertData(sname string, rec map[string]any, attrs, sep, div string, update
 	} else {
 		log.Printf("ERROR: unable to create globus link %v", err)
 	}
+	// add doi attributes
+	doiAttributes := []string{"doi", "doi_url", "doi_user", "doi_created_at", "doi_public", "doi_provider", "doi_access_metadata"}
+	for _, attr := range doiAttributes {
+		if attr == "doi_public" {
+			rec[attr] = false
+		} else {
+			rec[attr] = ""
+		}
+	}
+
 	// generate unique id
 	didValue, ok := rec["did"]
 	did := fmt.Sprintf("%s", didValue)
