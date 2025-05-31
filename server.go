e30973a6e00956b9a79fe925c22a61c0d51e9a7a
Valentin Kuznetsov <vkuznet@gmail.com>
2025-05-31 09:07:35 -0400

    add new keys to skip list

--- server.go
+++ server.go
@@ -86,7 +86,7 @@ func Server() {
 	_skipKeys = srvConfig.Config.CHESSMetaData.SkipKeys
 	if len(_skipKeys) == 0 {
 		// default list
-		_skipKeys = []string{"user", "date", "description", "schema_name", "schema_file", "schema", "did", "doi", "doi_url", "doi_user", "doi_created_ad", "doi_public", "doi_access_metadata"}
+		_skipKeys = []string{"user", "date", "description", "schema_name", "schema_file", "schema", "did", "doi", "doi_url", "doi_user", "doi_created_ad", "doi_public", "doi_provider", "doi_access_metadata", "globus_link", "history"}
 	}
 
 	// setup web router and start the service
